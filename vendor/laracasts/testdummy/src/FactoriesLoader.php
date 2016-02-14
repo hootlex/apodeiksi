b35a239d41cbd7c22d297087215e7a4922f8f266
Alex Kyriakidis <hootlex@icloud.com>
2016-02-15 00:32:18 +0200

    use greek faker with testdummy factories

---
+++ vendor/laracasts/testdummy/src/FactoriesLoader.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Laracasts\TestDummy;
+
+class FactoriesLoader
+{
+
+    /**
+     * Load the factories.
+     *
+     * @param  string $basePath
+     * @return array
+     */
+    public function load($basePath)
+    {
+        $this->assertThatFactoriesDirectoryExists($basePath);
+
+        $designer = new Designer;
+        $faker = new FakerAdapter;
+
+        $faker->locale('el_GR');
+
+        $factory = function ($name, $shortName, $attributes = []) use ($designer, $faker) {
+            return $designer->define($name, $shortName, $attributes);
+        };
+
+        foreach ((new FactoriesFinder($basePath))->find() as $file) {
+            include($file);
+        }
+
+        return $designer->definitions();
+    }
+
+    /**
+     * Assert that the given factories directory exists.
+     *
+     * @param  string $basePath
+     * @return mixed
+     * @throws TestDummyException
+     */
+    private function assertThatFactoriesDirectoryExists($basePath)
+    {
+        if ( ! is_dir($basePath)) {
+            throw new TestDummyException(
+                "The path provided for the factories directory, {$basePath}, does not exist."
+            );
+        }
+    }
+}
